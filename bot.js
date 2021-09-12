a3f9bdd332385a669c7b375d22a766e8a99e85ad
Kaweebro <90501601+Kaweebro@users.noreply.github.com>
2021-09-12 08:22:35 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,7 +97,7 @@ async function Alexa () {
     conn.on('connecting', async () => {
         console.log(`${chalk.green.bold('WhatAlexa')}
 ${chalk.white.bold('Version:')} ${chalk.red.bold(config.VERSION)}
-${chalk.blue.italic('Made By TOXIC-DEVIL')}`);
+${chalk.blue.italic('Made By Kaweesha Chamod')}`);
     });
     
 
