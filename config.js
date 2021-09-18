7e06f833dac14776077522bb7dcee76fdced8538
xmain12 <90620251+xmain12@users.noreply.github.com>
2021-09-18 21:15:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ module.exports = {
     BRANCH: 'master',
     DEVELOPER: 'Kaweesha Chamod',
     AUTHOR: 'Kaweesha Chamod',
-    GIT: 'https://github.com/xmain12/Stefanie_.git',
+    GIT: 'https://github.com/Kaweeshachamodk/stefanie',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
